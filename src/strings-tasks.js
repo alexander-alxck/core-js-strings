b35a3fdf1ca30e1953b083d44a2d3e27dcdca153
alexander-alxck <alexander.alexeichik28@gmail.com>
2026-09-19 17:52:50 +0300

    feat: add invertCase

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -400,8 +400,13 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str
+    .split('')
+    .map((item) =>
+      item === item.toLowerCase() ? item.toUpperCase() : item.toLowerCase()
+    )
+    .join('');
 }
 
 /**
